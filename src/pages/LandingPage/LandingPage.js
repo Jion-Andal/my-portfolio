8f2274162c030872baa3cce690e753c14f751cde
jion-andal <andaljion@gmail.com>
2025-09-04 01:08:25 +0800

    Testing changes for deployment

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ export function LandingPage() {
   return (
     <div>
       <h1>Welcome to the Landing Page</h1>
-      <p>This is the main entry point of the application.</p>
+      <p>This is the main entry point of the application. Testing.</p>
     </div>
   );
 }
